224abe0f1b533f3b0ef7f98ed3ddb7a5ff6a1a9d
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2022-02-13 13:56:43 +0100

    g

--- WebAssembly/RustFrondEnd/src/main.rs
+++ WebAssembly/RustFrondEnd/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use prelude::prelude::*;
+
+
+/* Model */
+struct Model {
+    value: i64
 }
+
+#[function_component(App)]
+fn app() => Html {
+    let state = use_state(|| Model {
+        value: 0
+    });
+
+    let onclick = {
+        let state = state.clone();
+
+        Callback::from(move |_| {
+            state.set(Model {
+                value: state.value + 1
+            })
+        })
+    }
+
+    html! {
+        <div>
+            <button {onclick}>{ "+1" }</button>
+            <p>{ state.value }</p>
+        </div>
+    }
+}
+
