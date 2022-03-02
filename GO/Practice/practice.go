8fa6227fd3bf672b74c39d3f8c46690a55e8ab38
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2022-03-02 23:39:30 +0100

    f

--- GO/Practice/practice.go
+++ GO/Practice/practice.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math"
+	"runtime"
+	"time"
 )
 
 func main() {
@@ -40,6 +42,9 @@ func main() {
 	infinitearray := [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 
 	// slice array
+	//
+	// A slice is formed by specifying two indices, a low and high bound, separated by a colon:
+	// a[low : high]
 	atest := infinitearray[:]   // slice of all elements
 	btest := infinitearray[3:]  // slice from 4th element to end
 	ctest := infinitearray[:6]  // slice first 6 elements
@@ -65,6 +70,9 @@ func main() {
 	// ----------------------------
 	// Maps
 	// ----------------------------
+	/*
+		Map literals are like struct literals, but the keys are required.
+	*/
 
 	// create a map:	 key -  value
 	mapTest := make(map[string]string)
@@ -84,7 +92,7 @@ func main() {
 
 	}
 
-	// While loop
+	// While loop / For Continued loop
 	n := 1
 	for n < 5 {
 		n *= 2
@@ -114,6 +122,17 @@ func main() {
 	// ----------------------------
 	// Pointers
 	// ----------------------------
+	/*
+		Go has pointers. A pointer holds the memory address of a value.
+	*/
+
+	var ptest *int
+
+	itest := 42
+	ptest = &itest
+
+	fmt.Println(*ptest) // read i through the pointer p
+	*ptest = 21         // set i through the pointer p
 
 	var year int = 2021
 	var p *int = &year
@@ -128,33 +147,141 @@ func main() {
 
 	// Verbs
 	// ------
-	// %v - It takes pretty much any variable type and formats it in the default style for that type.
-	// %s - Simply a string. This verb doesn’t interpret the string that’s passed in and so it shows up as is.
-	// %q - This verb formats values (i.e., arguments) with quotes
-	// %d - formats a regular old base-10 integer number.
-	// %t - formats a boolean value
-	// %b - formats binary
-	// %c - formats character
-	// %e - formats float
-	// %p - formats pointer value
+	/*
+		%v - It takes pretty much any variable type and formats it in the default style for that type.
+		%s - Simply a string. This verb doesn’t interpret the string that’s passed in and so it shows up as is.
+		%q - This verb formats values (i.e., arguments) with quotes
+		%d - formats a regular old base-10 integer number.
+		%t - formats a boolean value
+		%b - formats binary
+		%c - formats character
+		%e - formats float
+		%p - formats pointer value
+		%T - print out the type
+	*/
+
+	// ----------------------------
+	// Structs
+	// ----------------------------
+	/*
+		A struct is a collection of fields.
+		Struct fields are accessed using a dot.
+		Struct fields can be accessed through a struct pointer.
+	*/
+
+	type Vertex2 struct {
+		X int
+		Y int
+	}
+
+	// ----------------------------
+	// Switch
+	// ----------------------------
+
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+	}
+
+	// Switch type / switch with no condition
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
 
 	// ----------------------------
 	// Methods
 	// ----------------------------
-	// Go does not have classes. However, you can define methods on types.
-	// A method is a function with a special receiver argument.
-	// The receiver appears in its own argument list between the func keyword and the method name.
-	// In this example, the Abs method has a receiver of type Vertex named v.
+	/*
+		Go does not have classes. However, you can define methods on types.
+		A method is a function with a special receiver argument.
+		The receiver appears in its own argument list between the func keyword and the method name.
+		In this example, the Abs method has a receiver of type Vertex named v.
+	*/
 
 	v := Vertex{3, 4}
 	fmt.Println(v.Abs())
 
+	// ----------------------------
+	// Defer
+	// ----------------------------
+	/*
+		A defer statement defers the execution of a function until the surrounding function returns.
+		Will basicly print hello before world
+	*/
+	defer fmt.Println("world")
+
+	fmt.Println("hello")
+
+	// ----------------------------
+	// Interfaces
+	// ----------------------------
+	/*
+		An interface type is defined as a set of method signatures.
+		A value of interface type can hold any value that implements those methods.
+	*/
+
+	// ----------------------------
+	// Goroutines
+	// ----------------------------
+	/*
+		A goroutine is a lightweight thread managed by the Go runtime.
+		Goroutines run in the same address space, so access to shared memory must be synchronized. The sync package provides useful primitives, although you won't need them much in Go as there are other primitives
+	*/
+
+	say := func(s string) {
+		for i := 0; i < 5; i++ {
+			time.Sleep(100 * time.Millisecond)
+			fmt.Println(s)
+		}
+	}
+	go say("world")
+	say("hello")
+
+	/*
+		hello
+		world
+		hello
+		world
+		world
+		hello
+		hello
+		world
+		world
+		hello
+	*/
+
+	// ----------------------------
+	// Channels
+	// ----------------------------
+	/*
+		Channels are a typed conduit through which you can send and receive values with the channel operator, <-.
+		(The data flows in the direction of the arrow.)
+		Like maps and slices, channels must be created before use:
+		ch := make(chan int)
+		By default, sends and receives block until the other side is ready. This allows goroutines to synchronize without explicit locks or condition variables.
+	*/
+
 }
 
+// Methods
 type Vertex struct {
 	X, Y float64
 }
 
+// Method		name - return type
 func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
---
+++ GO/SQL Querying/README.md
@@ -0,0 +1,2 @@
+https://gowebexamples.com/mysql-database/
+https://pkg.go.dev/github.com/denisenkom/go-mssqldb
