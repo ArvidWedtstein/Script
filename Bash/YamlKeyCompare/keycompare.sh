2e176868ba8723e44d65743903c56c797c246396
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2023-02-19 15:11:18 +0100

    d

--- Bash/YamlKeyCompare/keycompare.sh
+++ Bash/YamlKeyCompare/keycompare.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Set the two file paths
 file1=./en-test.yaml
 file2=./no-test.yaml
 output_file=./diff.txt
@@ -11,12 +10,11 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 
-# Read the files and store the contents as strings
 contents1=$(<"$file1")
 contents2=$(<"$file2")
 
 
-# Function to get all keys and subkeys
+
 get_keys() {
   local content="$1"
   local prefix="$2"
@@ -25,21 +23,17 @@ get_keys() {
   local output
 
   while read -r line; do
-    # Check if line starts with a key
     if [[ $line =~ ^([a-zA-Z0-9_-]+): ]]; then
       key="${BASH_REMATCH[1]}"
       value="${line#$key: }"
-      # If value is empty, it is a subkey
       if [[ -z $value ]]; then
         # Add prefix if it exists
         if [[ -n $prefix ]]; then
           key="$prefix.$key"
         fi
-        # Call function recursively to get subkeys
         subkeys=$(get_keys "$content" "$key")
         output="$output $subkeys"
       else
-        # Add prefix if it exists
         if [[ -n $prefix ]]; then
           key="$prefix.$key"
         fi
@@ -51,15 +45,12 @@ get_keys() {
   echo "$output"
 }
 
-# Get all the keys and subkeys from both files
 keys1=$(get_keys "$contents1")
 keys2=$(get_keys "$contents2")
 
-# Loop through all the keys and subkeys in the first file
+
 for key in $keys1; do
-  # Check if the key exists in the second file
   if grep -qwF "$key:" <<< "$contents2"; then
-    # If the key exists in both files, compare the values
     value1=$(awk "/^$key: /{print; exit}" "$file1" | cut -d ' ' -f2-)
     value2=$(awk "/^$key: /{print; exit}" "$file2" | cut -d ' ' -f2-)
     if [[ "$value1" == "$value2" ]]; then
@@ -73,9 +64,7 @@ for key in $keys1; do
   fi
 done
 
-# Loop through all the keys and subkeys in the second file
 for key in $keys2; do
-  # Check if the key exists in the first file
   if ! grep -qwF "$key:" <<< "$contents1"; then
     echo -e "$key: ${RED}XX${NC}" # does not exist in first file
     echo "$key" >> "$output_file"
